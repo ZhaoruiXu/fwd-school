e37bfc8b73bed92556e0a8b9018155f0e60e65b7
Zhaorui Xu <xzr0429@gmail.com>
2022-05-28 21:42:47 -0700

    add footer style and content

--- footer.php
+++ footer.php
@@ -13,17 +13,20 @@
 
 	<footer id="colophon" class="site-footer">
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'fwd-school' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'fwd-school' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
+			<p>
+				
+					<?php
+					/* translators: %s: CMS name, i.e. WordPress. */
+					printf( esc_html__( 'Created by %s' ), '<a href= "https://wordpress.org/">Rui Xu</a>' );
+					?>
+				
+			</p>
+			<p>
 				<?php
 				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'fwd-school' ), 'fwd-school', '<a href="http://zhaoruixu.com">Rui Xu</a>' );
+				printf( esc_html__( 'Photos courtesy of %s.' ), '<a href="https://burst.shopify.com/">Burst</a>' );
 				?>
+			</p>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
